6fc6e04564c0c5af9ee52bcb83f5789c2ca16956
luismbmelo <luismbmelo@gmail.com>
2022-10-22 14:43:17 +0200

    exposing react build

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build')
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(use_reloader=True, port=5000, threaded=True)
