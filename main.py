341a8a405406efcd214fbc8d76a395efd1122292
luismbmelo <luismbmelo@gmail.com>
2022-10-22 17:49:45 +0200

    fixing sql

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import os
 from flask import Flask, render_template
 import psycopg2
 
+app = Flask(__name__, static_url_path='',
+                  static_folder='react',
+                  template_folder='react')
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
+
+
+
+
 #connect to the db
 con = psycopg2.connect(
     host = "containers-us-west-50.railway.app",
@@ -20,11 +35,3 @@ cur.execute("SELECT * FROM attendees")
 
 #close connection to the db
 con.close()
-
-@app.route("/")
-def hello():
-    return render_template("index.html")
-
-
-if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True)
