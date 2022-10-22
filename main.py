39b881a449dbc6fe3e1a0d264706bcfaedbbeca2
luismbmelo <luismbmelo@gmail.com>
2022-10-22 17:47:23 +0200

    adding psycopg2

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 from flask import Flask, render_template
+import psycopg2
 
+#connect to the db
+con = psycopg2.connect(
+    host = "containers-us-west-50.railway.app",
+    database = "railway",
+    user = "postgres",
+    password = "Yqew4AGn38m9TpdNHzsW",
+    port = "7453"
+)
 app = Flask(__name__, static_url_path='',
                   static_folder='react',
                   template_folder='react')
+
+cur = con.cursor()
+
+cur.execute("SELECT * FROM attendees")
+
+#close connection to the db
+con.close()
 
 @app.route("/")
 def hello():
