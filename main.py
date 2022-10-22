2df2d63c3e5220e7aaa738ba020e4bc826139dc3
luismbmelo <luismbmelo@gmail.com>
2022-10-22 14:10:05 +0200

    deploying react build

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to capsula's Flask app 🚅"})
-
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
