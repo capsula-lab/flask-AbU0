46f913ed30db58fa05271d6003a3d31910d4a506
luismbmelo <luismbmelo@gmail.com>
2022-10-22 18:03:43 +0200

    write to file solution, dropping sql

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-import psycopg2
+
 app = Flask(__name__, static_url_path='',
                   static_folder='react',
                   template_folder='react')
@@ -14,22 +14,9 @@ if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
 
 
-
-#connect to the db
-con = psycopg2.connect(
-    host = "containers-us-west-50.railway.app",
-    database = "railway",
-    user = "postgres",
-    password = "Yqew4AGn38m9TpdNHzsW",
-    port = "7453"
-)
-app = Flask(__name__, static_url_path='',
-                  static_folder='react',
-                  template_folder='react')
-
-cur = con.cursor()
-
-cur.execute("SELECT * FROM attendees")
-
-#close connection to the db
-con.close()
+@app.route('/api/<name>')
+def create_task(name):
+    f = open("guests.txt", "a")
+    f.write(name)
+    f.close()
+    return "alles gut"
