efcd4ca9219f51a725eabb674c5b3e782e0cf0d2
luismbmelo <luismbmelo@gmail.com>
2022-10-22 18:09:51 +0200

    get endpoint for guests v2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_url_path='',
                   static_folder='react',
@@ -8,11 +8,6 @@ app = Flask(__name__, static_url_path='',
 @app.route("/")
 def hello():
     return render_template("index.html")
-
-
-if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True)
-
 
 @app.route('/api/<name>')
 def create_task(name):
@@ -23,7 +18,13 @@ def create_task(name):
 
 @app.route('/api/get/getTheGuests')
 def create_task(name):
-    f = open("guests.txt", "a")
+    f = open("guests.txt", "r")
     guests = f.read()
     f.close()
-    return guests
+    return jsonify(guests)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
+
+
