b19b15abb38634f647683e385a42c4a3da114a8e
luismbmelo <luismbmelo@gmail.com>
2022-10-22 15:18:55 +0200

    new react serving2

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='',
-                  static_folder='frontend/build',
-                  template_folder='frontend/build')
+                  static_folder='',
+                  template_folder='')
 
 @app.route("/")
 def hello():
