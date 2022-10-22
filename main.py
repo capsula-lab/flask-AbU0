749912e9c5f7849b76b7fed81c288d45bcfef6ce
luismbmelo <luismbmelo@gmail.com>
2022-10-22 14:39:18 +0200

    just including react, flask untouched

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory('frontend/build/index.html')
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
