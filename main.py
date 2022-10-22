6b427f202033a95ac3ec9a2f6af52df07c8c96c8
luismbmelo <luismbmelo@gmail.com>
2022-10-22 14:33:02 +0200

    again

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory('frontend/build/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
